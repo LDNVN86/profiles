55783478594697d9d808f4dde1ac28904263b6df
LDN <godskyfastmc@gmail.com>
2024-08-13 00:08:13 +0700

    hello9

--- java.js
+++ java.js
@@ -6,9 +6,19 @@ menuToggle.addEventListener("click", () => {
     sidebar.style.animation = "slideOut 0.3s forwards";
     setTimeout(() => {
       sidebar.classList.remove("open");
-    }, 200); 
+    }, 200);
   } else {
     sidebar.classList.add("open");
     sidebar.style.animation = "slideIn 0.3s forwards";
   }
 });
+
+function setTime() {
+  const now = new Date();
+  const hour = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const timeClock = `${hour}:${minutes}:${seconds}`;
+  document.getElementById("clock").innerHTML=timeClock;
+}
+setInterval(setTime,1000);
