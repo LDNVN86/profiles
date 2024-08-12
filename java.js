b3bb4c1017564f6bd792a0647f22b08964d22ff2
LDN <godskyfastmc@gmail.com>
2024-08-13 00:22:27 +0700

    fixed

--- java.js
+++ java.js
@@ -18,7 +18,7 @@ function setTime() {
   const hour = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
-  const timeClock = `${hour}:${minutes}:${seconds}`;
+  const timeClock = `${hour} : ${minutes} : ${seconds}`;
   document.getElementById("clock").innerHTML=timeClock;
 }
 setInterval(setTime,1000);
